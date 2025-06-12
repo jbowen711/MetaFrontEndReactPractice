bbaabdd25af81f27016be03a2e8504dca4942def
Jonathan Bowen <jonathanbowen7@gmail.com>
2025-06-12 12:25:17 -0400

    contextAPI

---
+++ AdvancedReactMeta/contextAPI/src/ThemeContext.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { createContext } from "react";
+import { useState } from "react";
+
+const ThemeContext = createContext(undefined);
+
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+
+}
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error("useTheme must be used within a ThemeProvider");
+    }
+    return context;
+}
+
+
+
